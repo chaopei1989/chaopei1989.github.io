bf74305037a5a58481a385d300cb7e1b6731ae36
chaopei1989 <chaopei1989@gmail.com>
2020-08-04 21:41:18 +0800

    comment for leetcode

---
+++ codes/leetcode/course-schedule-207/course-schedule-207.m
@@ -0,0 +1,19 @@
+
+@interface Solution : NSObject {
+
+}
+
+- (void)viewDidLoad;
+
+@end
+
+
+
+@implementation Solution
+
+- (void)viewDidLoad {
+    // Do any additional setup after loading the view.
+	Solution *inst = [[Solution alloc]init];
+}
+
+@end
--- codes/leetcode/flatten-binary-tree-to-linked-list-114/flatten-binary-tree-to-linked-list-114.go
+++ codes/leetcode/flatten-binary-tree-to-linked-list-114/flatten-binary-tree-to-linked-list-114.go
@@ -1,12 +1,31 @@
-/**
- * 给定一个二叉树，原地将它展开为一个单链表。
- * 注意需要按前序顺序
- */
+/*114. 二叉树展开为链表
+题目链接: https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/
+
+给定一个二叉树，原地将它展开为一个单链表。
+
+例如，给定二叉树
+    1
+   / \
+  2   5
+ / \   \
+3   4   6
+
+将其展开为：
+1
+ \
+  2
+   \
+    3
+     \
+      4
+       \
+        5
+         \
+          6
+*/
 package main
 
-/**
- * Definition for a binary tree node.
- */
+// TreeNode represents as a binary tree node.
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
